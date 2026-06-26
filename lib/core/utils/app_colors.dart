f00f75a38c86cb864dae384a4128d10df5840774
bsher morjan <bshermorgan4@gmail.com>
2026-06-26 18:14:55 +0300

    dots indicator

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff1f5e3b);
+  // static const Color secondaryColor = Color(0xffe91e63);
+}
