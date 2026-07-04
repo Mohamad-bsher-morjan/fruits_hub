19b4a1243d9e173f96d5ad457e94b15e05921308
bsher morjan <bshermorgan4@gmail.com>
2026-07-04 17:21:48 +0300

    add text form field

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/custom_text_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -8,7 +9,17 @@ class LoginViewBody extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          
+          SizedBox(height: 24),
+          CustomTextFormField(
+            hintText: 'البريد الالكتروني',
+            textInputType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            suffixIcon: Icon(Icons.remove_red_eye,color: Color(0xffc9cecf),),
+            hintText: 'كلمة المرور',
+            textInputType: TextInputType.visiblePassword,
+          ),
         ],
       ),
     );
