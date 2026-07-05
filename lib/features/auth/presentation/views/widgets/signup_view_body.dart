6e4afb47643353efe85b77ed55f03e0de980348e
bsher morjan <bshermorgan4@gmail.com>
2026-07-05 12:18:31 +0300

    add text filed to signup view

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_text_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: .symmetric(horizontal: kHorizintalPadding),
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            CustomTextFormField(
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'البريد الالكتروني',
+              textInputType: TextInputType.emailAddress,
+              suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffc9cecf)),
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
