a0a4d1e26f0c644f11d1d2013757bba801a88b7e
bsher morjan <bshermorgan4@gmail.com>
2026-07-05 11:16:52 +0300

    add mutliple register choices

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_field.dart';
 import 'package:fruits_hub/core/widgets/or_divider.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/social_login_button.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -12,42 +15,70 @@ class LoginViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(height: 24),
-          CustomTextFormField(
-            hintText: 'البريد الالكتروني',
-            textInputType: TextInputType.emailAddress,
-          ),
-          SizedBox(height: 16),
-          CustomTextFormField(
-            suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffc9cecf)),
-            hintText: 'كلمة المرور',
-            textInputType: TextInputType.visiblePassword,
-          ),
-          SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: .end,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              Text(
-                'نسيت كلمة المرور؟',
-                style: AppStyles.semiBold13.copyWith(
-                  color: AppColors.lightPrimaryColor,
+              SizedBox(height: 24),
+              CustomTextFormField(
+                hintText: 'البريد الالكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 16),
+              CustomTextFormField(
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffc9cecf),
                 ),
+                hintText: 'كلمة المرور',
+                textInputType: TextInputType.visiblePassword,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: .end,
+                children: [
+                  Text(
+                    'نسيت كلمة المرور؟',
+                    style: AppStyles.semiBold13.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 33),
+              CustomButton(
+                backgroundColor: AppColors.primaryColor,
+                title: 'تسجيل دخول',
+                onPressed: () {},
+              ),
+
+              SizedBox(height: 33),
+              DontHaveAnAccountWidget(),
+              SizedBox(height: 33),
+              OrDivider(),
+              SizedBox(height: 15),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesGoogleIcon,
+                title: 'تسجيل بواسطة غوغل',
+              ),
+              SizedBox(height: 16),
+
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesApplIcon,
+                title: 'تسجيل بواسطة ابل',
+              ),
+              SizedBox(height: 16),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesFacebookIcon,
+                title: 'تسجيل بواسطة فيسبوك',
               ),
             ],
           ),
-          SizedBox(height: 33),
-          CustomButton(
-            backgroundColor: AppColors.primaryColor,
-            title: 'تسجيل دخول',
-            onPressed: () {},
-          ),
-
-          SizedBox(height: 33),
-          DontHaveAnAccountWidget(),
-          OrDivider()
-        ],
+        ),
       ),
     );
   }
---
+++ lib/features/auth/presentation/views/widgets/social_login_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:svg_flutter/svg.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  const SocialLoginButton({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.onPressed,
+  });
+  final String image;
+  final String title;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: Color(0xffdcdede), width: 1),
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        onPressed: onPressed,
+        child: ListTile(
+          visualDensity: VisualDensity(vertical: VisualDensity.minimumDensity),
+          leading: SvgPicture.asset(image),
+          title: Text(
+            title,
+            style: AppStyles.semiBold16,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
