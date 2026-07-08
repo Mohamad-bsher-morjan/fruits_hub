063870423f59168967b68f24dbec0a713ce9131c
bsher morjan <bshermorgan4@gmail.com>
2026-07-08 17:36:06 +0300

    create user with email and password fun && add dartz

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+}
