1a1addaa59d2e11be5a96ad7586efe9ab68fad73
bsher morjan <bshermorgan4@gmail.com>
2026-08-28 20:42:59 +0300

    impelement fetch user data part 1

--- lib/core/services/data_service.dart
+++ lib/core/services/data_service.dart
@@ -1,3 +1,6 @@
+import 'package:fruits_hub/features/auth/domain/entity/user_entity.dart';
+
 abstract class DatabaseService {
   Future<void> addData(String path, Map<String, dynamic> data);
+  Future<UserEntity> getUserData(String path, String uId);
 }
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruits_hub/features/auth/data/models/user_model.dart';
+import 'package:fruits_hub/features/auth/domain/entity/user_entity.dart';
 
 import 'data_service.dart';
 
@@ -7,5 +9,12 @@ class FireStoreService implements DatabaseService {
   @override
   Future<void> addData(String path, Map<String, dynamic> data) async {
     await firestore.collection(path).add(data);
+  }
+
+  @override
+  Future<UserEntity> getUserData(String path, String uId) async {
+    var data = await firestore.collection(path).doc(uId).get();
+
+    return UserModel.fromMap(data.data() as Map<String, dynamic>);
   }
 }
